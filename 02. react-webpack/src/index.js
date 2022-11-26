027a310a39ed9c7393897c1593314a69eee26fbc
tranlybuu <lybuu9117@gmail.com>
2022-11-26 09:36:19 +0700

    upload

--- 02. react-webpack/src/index.js
+++ 02. react-webpack/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react' // nạp thư viện react
-import ReactDOM from 'react-dom' // nạp thư viện react-dom
+import ReactDOM from 'react-dom/client' // nạp thư viện react-dom
 
 // Tạo component App
 function App() {
@@ -12,5 +12,10 @@ function App() {
 }
 
 // Render component App vào #root element
-ReactDOM.render(<App />, document.getElementById('root'))
+// React@17
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+// React@18
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
 
