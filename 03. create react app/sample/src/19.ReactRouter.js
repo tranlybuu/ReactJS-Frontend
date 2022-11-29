ee8132f9aca7786ec4c057b7d92e4bce56d08584
tranlybuu <lybuu9117@gmail.com>
2022-11-30 00:45:40 +0700

    upload

---
+++ 03. create react app/sample/src/19.ReactRouter.js
@@ -0,0 +1,45 @@
+import './App.css'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import HomePage from './pages/Home';
+import NewsPage from './pages/News';
+import ContactPage from './pages/Contact';
+
+
+function NewApp() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/news' element={<NewsPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+
+function App(){
+  return (
+    <div>
+      <BrowserRouter>
+        <NewApp />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ 03. create react app/sample/src/pages/News.js
@@ -0,0 +1,8 @@
+
+
+function New() {
+    return (
+        <h1>New Page</h1>
+    )
+}
+export default New
